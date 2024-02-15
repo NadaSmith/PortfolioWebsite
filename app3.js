686cae44e2be5b4f0bfdae370712df4265a3f90c
Nada Smith <103263245+NadaSmith@users.noreply.github.com>
2024-02-15 13:22:18 -0500

    Update app3.js
    Updating file.

--- app3.js
+++ app3.js
@@ -1,11 +1,4 @@
-let typed = new Typed(".title",{
-    strings:["Software Engineer", "Front-End Developer", "Web Developer", "Tech Blogger"],
-    typedSpeed: 70,
-    backSpeed: 55,
-    loop: true
-})
-
-let moon = document.getElementById("moon");
+var moon = document.getElementById("moon");
 
 moon.onclick = function() {
     if (document.body.classList.toggle("dark-theme")){
